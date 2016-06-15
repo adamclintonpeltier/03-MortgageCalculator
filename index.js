a8576fd4c147453b1f849b5ac56f825f1a109f3c
Adam Clinton Peltier <adamclintonpeltier@Adams-MacBook-Air.local>
2016-06-15 15:13:19 -0700

    Second Commit

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-//button 
-function calculator(){
 
-//variables for input fields
-	var loanBalance = $("#loanBalance").val() ;
-	var interestRate = $("#interestRate").val() ;
-	var loanTerm = $("#loanTerm").val() ;
-	var period = $("#period option:selected").val() ;
+
+// Defining button. 
+
+function calculator() {
+
+	//values taken from the input fields.
+
+	var loanBalance = $("#loanBalance").val();
+	var interestRate = $("#interestRate").val();
+	var loanTerm = $("#loanTerm").val();
+	var period = $("#period option:selected").val();
+
+	// Equations for processing results.
 
 	var numberOfPayments = loanTerm * period;
-	var monthlyInterestRate = (interestRate/100)/period;
+	var monthlyInterestRate = (interestRate / 100) / period;
 	var compoundedInterestRate = Math.pow((1 + monthlyInterestRate), numberOfPayments);
-	var interestQuotient = (monthlyInterestRate * compoundedInterestRate)/(compoundedInterestRate-1);
-	
-	monthlyPayment = Math.round((loanBalance * interestQuotient) * 100)/100;
+	var interestQuotient = (monthlyInterestRate * compoundedInterestRate) / (compoundedInterestRate - 1);
+
+	// Results: Your total monthly payment.	
+
+	monthlyPayment = Math.round((loanBalance * interestQuotient) * 100) / 100;
 	$("#monthlyPayment").html(" Your Monthly Payment is $" + monthlyPayment);
 
 }
+
+// Initialed By: RN, MTS, AP.
